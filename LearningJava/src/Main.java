64ec8190a41e78ca0066167a54d6d52ce347c36a
Roger Perez <ropere@microsoft.com>
2024-02-24 15:36:46 -0800

    add if condition

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -4,27 +4,13 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int studentAge = 35;
-        double gpa = 3.50;
-        String firstName = "Roger";
-        String lastName = "Perez";
+        Scanner scanner = new Scanner(System.in);
+        int inputtedNum = scanner.nextInt();
 
-        char firstInitial = firstName.charAt(0);
-        char lastInitial = lastName.charAt(0);
-
-        System.out.println(studentAge);
-        System.out.println(gpa);
-        System.out.println(firstName);
-        System.out.println(lastName);
-
-        System.out.println(firstInitial);
-        System.out.println(lastInitial);
-
-        System.out.println(firstName + " " + lastName + " has a gpa of " + gpa);
-        Scanner input = new Scanner(System.in);
-
-        gpa = input.nextDouble();
-
-        System.out.println(firstName + " "+ lastName + " no has a GPA of " + gpa);
+        if (inputtedNum < 5) {
+            System.out.println("Enjoy life");
+        } else {
+            System.out.println("Hate life");
+        }
     }
 }
