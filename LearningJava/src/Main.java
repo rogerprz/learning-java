a91f7556ade97cded67e4280f88bab3fea8cb71e
Roger Perez <ropere@microsoft.com>
2024-02-24 14:37:42 -0800

    Test charAt

---
+++ LearningJava/src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int studentAge = 35;
+        double gpa = 3.50;
+        String firstName = "Roger";
+        String lastName = "Perez";
+
+        char firstInitial = firstName.charAt(0);
+        char lastInitial = lastName.charAt(0);
+
+        System.out.println(studentAge);
+        System.out.println(gpa);
+        System.out.println(firstName);
+        System.out.println(lastName);
+
+        System.out.println(firstInitial);
+        System.out.println(lastInitial);
+
+
+    }
+}
