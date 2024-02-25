fe6bc40757089ed6d9a1f6605f7f60ee391b2691
Roger Perez <ropere@microsoft.com>
2024-02-25 12:04:41 -0800

    tea time

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static void announceDeveloperTeaTime() {
+        System.out.println(("Waiting for tea time"));
+        System.out.println("Type a random word..:");
+        Scanner input = new Scanner(System.in);
+
+        input.next();
+        System.out.println("It's tea time");
+    }
     public static void main(String[] args) {
        String question = "What is your favorite color";
        String choice1 = "blue";
-       String choice2 = "red";
-       String choice3= "green";
 
-       Scanner input = new Scanner(System.in);
-       String answer = input.next();
+       announceDeveloperTeaTime();
     }
 }
