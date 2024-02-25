411c7e2601c2c2083c438755e34568a976363332
Roger Perez <ropere@microsoft.com>
2024-02-25 12:00:16 -0800

    Add challenge

--- LearningJava/src/Main.java
+++ LearningJava/src/Main.java
@@ -9,23 +9,7 @@ public class Main {
        String choice2 = "red";
        String choice3= "green";
 
-       String correctAnswer = choice2;
-       boolean guessedCorrectAnswer = true;
-       while (guessedCorrectAnswer) {
-           System.out.println(question);
-           Scanner input = new Scanner(System.in);
-           String answer = input.next();
-
-           if (answer.equals(choice1.toLowerCase())) {
-               System.out.println(("Good try, but " + choice1) + " is not correct");
-
-           } else if (answer.equals(choice2)) {
-               System.out.println(("Great, " + choice2) + " is correct");
-               guessedCorrectAnswer = false;
-           } else if (answer.equals(choice3)) {
-               System.out.println(("Good try, but " + choice3) + " is not correct");
-           }
-       }
-       System.out.println("END PROGRAM");
+       Scanner input = new Scanner(System.in);
+       String answer = input.next();
     }
 }
